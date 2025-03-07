d42d6f136f8121c238c8ee2a31a722dfa6db29eb
Naman rai <namanrai309@gmail.com>
2025-03-07 12:57:32 +0530

    ok

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import LinkedInBot from "@/components/LinkedinBot";
 
 function Page() {
   const [linkedinUrl, setLinkedinUrl] = useState("");
